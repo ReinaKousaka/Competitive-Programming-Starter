c52b0e95418395b57be98d5587f91ce4dcc3c3b4
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-11-07 15:42:30 -0500

    LC2463

--- 2463/2463.py
+++ 2463/2463.py
@@ -1,15 +1,14 @@
 from typing import *
 import numpy as np
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linear_sum_assignment.html
 from scipy.optimize import linear_sum_assignment
 
 class Solution:
     def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
-        adjacent = []
+        cost = []
         for pos_f, limit in factory:
             row = [abs(pos_r - pos_f) for pos_r in robot]
             for _ in range(limit):
-                adjacent.append(row)
-        adjacent = np.array(adjacent)
-        row_idx, col_idx = linear_sum_assignment(adjacent)
-        return adjacent[row_idx, col_idx].sum()
+                cost.append(row)
+        cost = np.array(cost)
+        row_idx, col_idx = linear_sum_assignment(cost)
+        return cost[row_idx, col_idx].sum()
